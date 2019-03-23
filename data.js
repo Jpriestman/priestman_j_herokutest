ce2cfa1875132b3fc6ad7def3e70e26e65baaf1f
Jordan Priestman <jordanpriestman@Jordans-MacBook-Pro.local>
2019-03-22 20:37:11 -0400

    Finished product files

--- data.js
+++ data.js
@@ -1,21 +1,7 @@
 const data = {
-    name: "Jordan",
-    role: "Graphic Designer",
-
-    user1: {
-        name: "user1",
-        role: "designer",
-        bio: "some generic bio",
-        bioPic: "bioPic.jpg"
-    },
-
-    user2: {
-        name: "user2",
-        role: "developer",
-        bio: "some generic bio for user 2",
-        bioPic: "bioPic.jpg"
-    },
-
+    logo: "logo.svg",
+    bio: "Find your friends: view each others live location on a private map!",
+    productImg: "iphone.png"
 }
 
 // exports it so we can use it on a page
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,17 @@ const data = require('../data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Your First Express App', message: 'Express is actually pretty easy and could replace vue' });
+  res.render('index', { logo: 'Waves Logo', bio: 'Sign up for free today and connect your friends', productImg: "product image" });
 });
 
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 // Copied and pasted above route - get a dynamic user via the route parameter
 // Changed the / to /:user to grab a user added a conole log and chnaged user render page
-router.get('/:user', function(req, res, next) {
-  console.log('user:', data[req.params.user])
-  res.render('userpage', data[req.params.user]);
-});
+//router.get('/:user', function(req, res, next) {
+//  console.log('user:', data[req.params.user])
+//  res.render('userpage', data[req.params.user]);
+//});
 
 module.exports = router;
